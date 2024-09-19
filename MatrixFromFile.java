f68177554c98efa9b40fea4382fbc5487c3158f3
MarciusMoraes <marcius.vargas@acad.pucrs.br>
2024-09-19 17:27:28 -0300

    this is working

--- MatrixFromFile.java
+++ MatrixFromFile.java
@@ -2,14 +2,18 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+
+
+
 public class MatrixFromFile {
+    public static char[][] matrix;
 
     public static void main(String[] args) {
-        String filePath = "casoe30.txt";
+        String filePath = args[0];
 
         try {
             //Método para ler o arquivo e retornar a matriz
-            char[][] matrix = readPatternFromFile(filePath);
+            matrix = readPatternFromFile(filePath);
 
 
         } catch (IOException e) {
@@ -20,42 +24,88 @@ public class MatrixFromFile {
     // Método que transforma arquivo em matriz.
     public static char[][] readPatternFromFile(String filePath) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(filePath));
-        int wayCounter=0;
-        int wayFinder=0;
-        char actualChar=' ';
-        int pathSum=0;
+        
+        
 
         // Ler a primeira linha que contém o número de linhas e colunas
         String[] dimensions = reader.readLine().split(" ");
         int rows = Integer.parseInt(dimensions[0]);  
         int cols = Integer.parseInt(dimensions[1]);  
+        int initialPos= 0;
 
-        // Criar a matriz
-        char[][] matrix = new char[rows][cols];
+        
+        matrix = new char[rows][cols];
 
-        // Ler o resto do arquivo e preencher a matriz
+        
         for (int i = 0; i < rows; i++) {
             String line = reader.readLine();
             for (int j = 0; j < line.length(); j++) {
                 matrix[i][j] = line.charAt(j);
-                if (line.charAt(j)=='#') {
-                    wayCounter++;
-                }
             }
         }
-        reader.close();
-        
-        
 
-        while (wayFinder<=wayCounter) {
-            wayFinder++;
+        reader.close();
+
+        for(int i=0;i<cols;i++){
+            if (matrix[rows-1][i]!=' ') {
+                initialPos=i;
+                break;
+            }
         }
 
+
+
+        
+        System.out.println("I Am Here:");
+        System.out.println(maxSum(initialPos, rows-1, matrix[rows-1][initialPos]));
 
         return matrix;
     }
 
-    // Método para imprimir a matriz
+    
+    public static int maxSum(int horiPosition,int verPosition, char direction){
+        int sum=0;
+        switch (matrix[verPosition][horiPosition]) {
+            case '#':
+                return 0;
+                
+            case '/':
+                return maxSum(horiPosition + 1, verPosition - 1, '/');
+
+            case '\\':
+                return maxSum( horiPosition - 1, verPosition - 1, '\\');
+
+            case '|':
+                return maxSum(horiPosition , verPosition - 1, '|');
+            case 'V':
+                return Math.max(maxSum(horiPosition + 1, verPosition - 1, '/'),
+                maxSum( horiPosition - 1, verPosition - 1, '\\'));    
+            case 'W': 
+                return Math.max(maxSum (horiPosition, verPosition - 1, '|'),
+                Math.max(maxSum( horiPosition + 1, verPosition - 1, '/'),
+                maxSum( horiPosition - 1, verPosition - 1, '\\')));
+            
+            default:
+                switch (direction) {
+                    case '/':
+                        sum= maxSum( horiPosition + 1, verPosition - 1, '/');
+                        break;
+                    case '\\':
+                        sum= maxSum( horiPosition - 1, verPosition - 1, '\\');
+                        break;
+                    case '|':
+                        sum= maxSum( horiPosition, verPosition - 1, '|');
+                        break;
+                    
+                }
+                
+                return Integer.parseInt("" + matrix[verPosition][horiPosition]) + sum;
+                
+        }
+    }
+
+
+
     public static void printMatrix(char[][] matrix) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
---
+++ Node.java
@@ -0,0 +1,15 @@
+public class Node {
+        int value;
+        Node left;    // Filho à esquerda
+        Node middle;  // Filho no meio
+        Node right;   // Filho à direita
+    
+        // Construtor do nó
+        public Node(int value) {
+            this.value = value;
+            this.left = null;
+            this.middle = null;
+            this.right = null;
+        }
+    }
+
